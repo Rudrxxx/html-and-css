e11526149285861100140a2181a5c22b4f0b74fb
Rudransh <message.rudransh@gmail.com>
2025-01-03 22:43:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleLike(button){
-    if (button.textContent === "❤️ Like") {
-        button.textContent = "💔 Unlike";
-        button.style.backgroundColor = "#e63939";
-    } else {
-        button.textContent = "❤️ Like";
-        button.style.backgroundColor = "#ff4d4d";
-    } 
-}
